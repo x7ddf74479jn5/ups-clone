7144dc7324558ca144247920ed62c0b049081534
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-09-12 16:16:25 +0900

    ✨ feat: TabNavigator

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -3,7 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { CustomersScreen } from "../screens/CustomersScreen";
 import { OrdersScreen } from "../screens/OrdersScreen";
 import { useNavigation } from "@react-navigation/native";
-
+import { Icon } from "@rneui/themed";
 export type TabStackParamList = {
   Customers: undefined;
   Orders: undefined;
@@ -21,7 +21,19 @@ export const TabNavigator = () => {
   }, []);
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#59C1CC",
+        tabBarInActiveTintColor: "gray",
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Customers") {
+            return <Icon name="users" type="entypo" color={focused ? "#59C1CC" : "gray"} />;
+          } else if (route.name === "Orders") {
+            return <Icon name="users" type="entypo" color={focused ? "#EB6A7C" : "gray"} />;
+          }
+        },
+      })}
+    >
       <Tab.Screen name="Customers" component={CustomersScreen} />
       <Tab.Screen name="Orders" component={OrdersScreen} />
     </Tab.Navigator>
