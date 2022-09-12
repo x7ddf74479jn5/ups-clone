05c53a117bba28b103bd3ef2c78ee64934b92970
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-09-12 22:34:01 +0900

    ✨ add: CustomerCard

---
+++ components/CustomerCard.tsx
@@ -0,0 +1,41 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { useCustomerOrders } from "../hooks/useCustomerOrders";
+import { useTailwind } from "tailwind-rn/dist";
+import { useNavigation } from "@react-navigation/native";
+import { CustomersScreenNavigationProps } from "../screens/CustomersScreen";
+import { Card, Icon } from "@rneui/themed";
+
+type Props = {
+  userId: string;
+  name: string;
+  email: string;
+};
+
+export const CustomerCard: React.FC<Props> = ({ email, name, userId }) => {
+  const { loading, orders, error } = useCustomerOrders(userId);
+  const tw = useTailwind();
+  const navigation = useNavigation<CustomersScreenNavigationProps>();
+
+  return (
+    <TouchableOpacity>
+      <Card containerStyle={tw("p-5 rounded-lg ")}>
+        <View>
+          <View style={tw("flex-row justify-between")}>
+            <View>
+              <Text style={tw("text-2xl font-bold")}>{name}</Text>
+              <Text style={[tw("text-sm"), { color: "#59C1CC" }]}>ID: {userId}</Text>
+            </View>
+
+            <View style={tw("flex-row items-center justify-end")}>
+              <Text style={{ color: "#59C1CC" }}>{loading ? "loading..." : `${orders.length} x`}</Text>
+              <Icon name="box" type="entypo" color="#59C1CC" style={tw("mb-5 ml-auto")} size={50} />;
+            </View>
+          </View>
+        </View>
+        <Card.Divider />
+        <Text>{email}</Text>
+      </Card>
+    </TouchableOpacity>
+  );
+};
--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -8,6 +8,9 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigator/RootNavigator";
 import { Image, Input } from "@rneui/themed";
 import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_CUSTOMERS } from "../graphql/queries";
+import { CustomerCard } from "../components/CustomerCard";
 
 export type CustomersScreenNavigationProps = CompositeNavigationProp<
   BottomTabNavigationProp<TabStackParamList, "Customers">,
@@ -18,6 +21,7 @@ export const CustomersScreen = () => {
   const tw = useTailwind();
   const navigation = useNavigation<CustomersScreenNavigationProps>();
   const [input, setInput] = useState("");
+  const { loading, data, error } = useQuery(GET_CUSTOMERS);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -41,6 +45,12 @@ export const CustomersScreen = () => {
         onChangeText={setInput}
         containerStyle={tw("bg-white pt-5 pb-0 px-10")}
       />
+
+      {data?.getCustomers
+        .filter((customer: CustomerList) => customer.value.name.includes(input))
+        .map(({ name: ID, value: { name, email } }: CustomerResponse) => (
+          <CustomerCard key={ID} email={email} name={name} userId={ID} />
+        ))}
     </ScrollView>
   );
 };
