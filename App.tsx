6feca126c076e227e348aa1e30f6efb40273e14b
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-09-12 14:17:38 +0900

    🎉 start

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
+import { TailwindProvider } from "tailwind-rn";
+import utilities from "tailwind.json";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // @ts-ignore TailwindProvider ims missing a type definition
+    <TailwindProvider utilities={utilities}>
+      <View></View>
+    </TailwindProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
