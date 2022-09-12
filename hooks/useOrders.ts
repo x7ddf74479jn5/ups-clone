c2ecb6067ad0db212f87ad4a298b97fac0850258
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-09-12 21:39:00 +0900

    ✨ feat: useOrders

---
+++ hooks/useOrders.ts
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_ORDERS } from "../graphql/queries";
+export const useCustomerOrders = () => {
+  const { data, loading, error } = useQuery(GET_ORDERS);
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    if (!data) return;
+
+    const orders: Order[] = data.getOrders.map(({ value }: OrderResponse) => ({
+      carrier: value.carrier,
+      createdAt: value.createdAt,
+      shippingCost: value.shippingCost,
+      trackingId: value.trackingId,
+      trackingItems: value.trackingItems,
+      Lat: value.Lat,
+      Lng: value.Lng,
+      Address: value.Address,
+      City: value.City,
+    }));
+
+    setOrders(orders);
+  }, [data]);
+
+  return { orders, loading, error };
+};
