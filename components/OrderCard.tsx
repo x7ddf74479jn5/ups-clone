c5583972a6c5599ed9a6ce4000a1676f46c5164d
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-09-13 16:58:10 +0900

    ✨ feat: OrderCard

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -1,14 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { Card, Icon } from "@rneui/themed";
+import { useTailwind } from "tailwind-rn";
 
 type Props = {
   item: Order;
 };
 
-export const OrderCard: React.FC<Props> = ({ order }) => {
+export const OrderCard: React.FC<Props> = ({ item }) => {
+  const tw = useTailwind();
+
   return (
-    <View>
-      <Text>OrderCard</Text>
-    </View>
+    <TouchableOpacity>
+      <Card containerStyle={tw("px-5 rounded-lg")}>
+        <View style={tw("flex-row justify-between items-center")}>
+          <View>
+            <Icon name="truck-delivery" color="#EB6A7C" type="material-community" />
+            <Text style={{ fontSize: 10 }}>{new Date(item.createdAt).toLocaleDateString()}</Text>
+          </View>
+
+          <View>
+            <Text style={[tw("text-gray-400"), { fontSize: 10 }]}>
+              {item.carrier}-{item.trackingId}
+            </Text>
+
+            <Text style={tw("text-gray-500 text-xl")}>{item.trackingItems.customer.name}</Text>
+          </View>
+
+          <View style={tw("flex-row items-center")}>
+            <Text style={[tw("text-sm"), { color: "#EB6A7C" }]}>{item.trackingItems.items.length} x</Text>
+            <Icon style={tw("ml-2")} name="box" type="feather" />
+          </View>
+        </View>
+      </Card>
+    </TouchableOpacity>
   );
 };
